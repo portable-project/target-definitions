c1a5a48b38c71a4d534262d6f542a41bcbeb1982
sergey <serhg.t@gmail.com>
2019-05-28 09:30:56 +0300

    POWER 9 fist commit
    TODO readme for tests

---
+++ definitions/research/POWER 9/readme.md
@@ -0,0 +1,8 @@
+Architecture POWER 9 (ISA v3.0) definition
+===============
+
+### See [power.target.pdsl](./power.target.pdsl)
+
+### Breif overview: [Report](./docs/report.pdf)
+
+#### Author: [Tabunschik Sergey](https://github.com/JustRevolt)
---
+++ definitions/research/POWER 9/tests/listing.asm
@@ -0,0 +1,17 @@
+b 11184810
+
+bc 16, 0, 0x2AAA
+bc 8,2,0x2AAA
+bc 8,22,0x2AAA
+bc 4,27,0x2AAA
+bcl 4,27,0x2AAA
+bc 4,2,0x2AAA
+bc 4,14,0x2AAA
+bcla 12,17,0x2AAA
+bcctrl 12,17,1
+
+creqv 25,25,25
+crxor 3,3,3
+crxor 15,15,15
+crnor 2,2,2
+crnor 22,18,18
